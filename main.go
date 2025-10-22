caee4a4dda5f4989e2ce32a83c42ecb07031e074
fengtiecheng <17615007230@163.com>
2025-10-22 11:28:06 +0800

    refactor(study/login.go, main.go): 重构登录测试逻辑并调整示例测试调用
    - 提取`login`函数，简化登录过程
    - 添加响应结构体和数据结构体
    - 更新请求头设置，使用Token进行授权
    - 调整`main.go`中示例测试的调用顺序和注释

--- main.go
+++ main.go
@@ -9,44 +9,44 @@ import (
 func main() {
 
 	//1.知识点学习测试
-	//studyTest()
+	studyTest()
 
 	//2.例子测试
-	exampleTest()
+	//exampleTest()
 }
 
 // studyTest 知识点学习测试
 func studyTest() {
 
-	//1.基础demo测试
-	study.BaseDemoTest()
-
-	//2.异常测试
-	study.ErrorTest()
+	////1.基础demo测试
+	//study.BaseDemoTest()
+	//
+	////2.异常测试
+	//study.ErrorTest()
 
 	//3.登录测试
 	study.LoginTest()
 
-	//4.最大深度测试
-	study.MaxDepthTest()
-
-	//5.并发爬取测试
-	study.Parallel()
-
-	//6.代理测试
-	study.ProxyTest()
-
-	//7.队列设置
-	study.QueueTest()
-
-	//8.随机延迟测试
-	study.RandomDelayTest()
-
-	//9.redis存储测试
-	study.RedisStorageTest()
-
-	//10.请求上下文测试
-	study.RequestCtxTest()
+	////4.最大深度测试
+	//study.MaxDepthTest()
+	//
+	////5.并发爬取测试
+	//study.Parallel()
+	//
+	////6.代理测试
+	//study.ProxyTest()
+	//
+	////7.队列设置
+	//study.QueueTest()
+	//
+	////8.随机延迟测试
+	//study.RandomDelayTest()
+	//
+	////9.redis存储测试
+	//study.RedisStorageTest()
+	//
+	////10.请求上下文测试
+	//study.RequestCtxTest()
 }
 
 // exampleTest 示例测试
--- study/login.go
+++ study/login.go
@@ -8,43 +8,114 @@ import (
 	"github.com/gocolly/colly"
 )
 
+// Response 响应结构体
+type Response struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data *Data  `json:"data"`
+}
+
+// Data 数据结构体
+type Data struct {
+	Privileges []string `json:"privileges"`
+	Nickname   string   `json:"nickname"`
+	Avatar     string   `json:"avatar"`
+	UserID     int      `json:"userId"`
+	GodMode    bool     `json:"godMode"`
+	Token      string   `json:"token"`
+}
+
 // LoginTest 登录测试
 func LoginTest() {
 
 	//1.创建采集器
 	c := colly.NewCollector()
 
-	//2.声明用户名密码
-	userAuth := map[string]string{
-		"username": "admin",
-		"password": "yuanjing@1234",
-	}
-
-	//3.将数据转换为 JSON 格式
-	jsonData, err := json.Marshal(userAuth)
+	//2.登录获取Token
+	token, err := login(c)
 	if err != nil {
-		log.Printf("error marshaling JSON: %v", err)
+		log.Printf("login error:%v", err)
 		return
 	}
 
-	//4.设置请求头为 JSON
+	//3.请求头设置Token
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("authorization", "Mon-Token "+token)
+	})
+
+	//4.响应回调
+	c.OnResponse(func(r *colly.Response) {
+		log.Printf("status:%d, body:%s", r.StatusCode, string(r.Body))
+	})
+
+	//5.声明请求参数,将数据转换为 JSON 格式
+	jsonData, err := parsePayload(map[string]string{
+		"pageNum":  "1",
+		"pageSize": "15",
+	})
+	if err != nil {
+		return
+	}
+
+	//6.访问网站
+	if err = c.PostRaw("http://localhost:3000/api/project/list", jsonData); err != nil {
+		log.Println("error visiting url:", err)
+	}
+}
+
+func login(c *colly.Collector) (string, error) {
+
+	//1.设置请求头为 JSON
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("Content-Type", "application/json")
 	})
 
-	//5.授权访问，使用 PostRaw 发送 JSON 数据
-	err = c.PostRaw("http://localhost:3000/api/user/login", jsonData)
-	if err != nil {
-		log.Printf("error posting JSON: %v", err)
-	}
+	//2.定义响应结构体
+	var resp Response
 
-	//6.登录请求之后的回调
+	//3.登录请求之后的回调
 	c.OnResponse(func(r *colly.Response) {
-		log.Println("response received", r.StatusCode)
+
+		//4.如果响应异常，返回
+		if r.StatusCode != 200 {
+			log.Println("response error")
+			return
+		}
+
+		//5.解析响应体
+		if err := json.Unmarshal(r.Body, &resp); err != nil {
+			log.Println("error parsing JSON:", err)
+			return
+		}
 	})
 
-	//7.访问网站
-	if err = c.Visit("http://localhost:3000/"); err != nil {
-		log.Println("error visiting url:", err)
+	//6.声明用户名密码,将数据转换为 JSON 格式
+	jsonData, err := parsePayload(map[string]string{
+		"username": "admin",
+		"password": "yuanjing@1234",
+	})
+	if err != nil {
+		return "", err
+	}
+
+	//7.授权访问，使用 PostRaw 发送 JSON 数据
+	if err = c.PostRaw("http://localhost:3000/api/user/login", jsonData); err != nil {
+		return "", err
+	}
+
+	//8.判定是否登录成功
+	if resp.Code != 0 {
+		return "", err
+	} else {
+		return resp.Data.Token, nil
+	}
+}
+
+// parsePayload 参数转换
+func parsePayload(pagePayload map[string]string) ([]byte, error) {
+	if jsonData, err := json.Marshal(pagePayload); err != nil {
+		return nil, err
+	} else {
+		return jsonData, nil
 	}
 }
